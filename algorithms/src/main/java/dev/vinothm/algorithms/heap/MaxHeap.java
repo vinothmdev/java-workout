d5ccf02ec796c529d02fbc32aeb16fffcadb9661
vinothmdev <vinoth.m.dev@gmail.com>
2021-09-03 11:16:16 -0400

    format and adding maxheap

--- algorithms/src/main/java/dev/vinothm/algorithms/heap/MaxHeap.java
+++ algorithms/src/main/java/dev/vinothm/algorithms/heap/MaxHeap.java
@@ -1,35 +1,56 @@
 package dev.vinothm.algorithms.heap;
 
-import java.util.List;
-import java.util.stream.IntStream;
-
 public class MaxHeap {
-  int value;
-  MaxHeap left;
-  MaxHeap right;
+	int[] value;
 
-  public MaxHeap(int value, MaxHeap left, MaxHeap right) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
-  }
+	public MaxHeap(int[] value) {
+		this.value = value;
+	}
 
-  public static int getParentOf(int index) {
-    return index / 2;
-  }
+	public int getParentOf(int index) {
+		return index / 2 - 1;
+	}
 
-  public static int getLeftOf(int index) {
-    return 2*index;
-  }
+	public int getLeftOf(int index) {
+		return 2 * index + 1;
+	}
 
-  public static int getRightOf(int index) {
-    return 2*index+1;
-  }
+	public int getRightOf(int index) {
+		return 2 * index + 2;
+	}
 
-  public static  boolean isMaxHeap(List<Integer> input) {
-    if (input == null) return false;
+	public void heapify(int at) {
+		int left = this.getLeftOf(at);
+		int right = this.getRightOf(at);
+		int largest = left < this.value.length && this.value[left] > this.value[at] ? left : at;
+		largest = right < this.value.length && this.value[right] > this.value[largest] ? right : largest;
+		if (largest != at) {
+			int temp = this.value[at];
+			this.value[at] = this.value[largest];
+			this.value[largest] = temp;
+			this.heapify(largest);
+		}
 
-    return !/*NOT*/IntStream.range(1,input.size())
-      .anyMatch(e -> input.get(MaxHeap.getParentOf(e)) < input.get(e));
-  }
+	}
+
+	public void heapify(int[] arr, int at) {
+		int left = this.getLeftOf(at);
+		int right = this.getRightOf(at);
+		int largest = left < arr.length && arr[left] > arr[at] ? left : at;
+		largest = right < arr.length && arr[right] > arr[largest] ? right : largest;
+		if (largest != at) {
+			int temp = arr[at];
+			arr[at] = arr[largest];
+			arr[largest] = temp;
+			this.heapify(arr, largest);
+		}
+
+	}
+
+	public void build(int[] arr) {
+		for (int i = arr.length / 2 - 1; i >= 0; i--) {
+			this.heapify(arr, i);
+		}
+	}
+
 }
--- algorithms/src/test/java/dev/vinothm/algorithms/heap/MaxHeapTest.java
+++ algorithms/src/test/java/dev/vinothm/algorithms/heap/MaxHeapTest.java
@@ -1,17 +1,24 @@
 package dev.vinothm.algorithms.heap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
 class MaxHeapTest {
 
+	MaxHeap maxHeap = new MaxHeap(new int[] { 16, 14, 10, 4, 7, 9, 3, 2, 8, 1 });
+
 	@Test
-	void isMaxHeapTest() {
-		assertEquals(MaxHeap.isMaxHeap(List.of(16,14,10,8,7,9,3,2,4,1)), true);
-		assertEquals(MaxHeap.isMaxHeap(List.of(16,14,10,8,7,9,3,2,4,10)), false);
+	void hepifyTest() {
+		maxHeap.heapify(3);
+		assertArrayEquals(maxHeap.value, new int[] { 16, 14, 10, 8, 7, 9, 3, 2, 4, 1 });
+	}
+
+	@Test
+	void build() {
+		int[] arr = new int[] { 1, 8, 2, 3, 9, 7, 4, 10, 14, 16 };
+		maxHeap.build(arr);
+		assertArrayEquals(new int[] { 16, 14, 7, 10, 9, 2, 4, 1, 3, 8 }, arr);
 	}
 
 }
