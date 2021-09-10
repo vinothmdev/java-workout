761b50ba979a7b292c5d4172409b907dd6eb0f8c
vinothmdev <vinoth.m.dev@gmail.com>
2021-09-10 16:56:04 -0400

    Tree refactoring

--- algorithms/src/main/java/dev/vinothm/algorithms/tree/Utils.java
+++ algorithms/src/main/java/dev/vinothm/algorithms/tree/Utils.java
@@ -1,32 +1,106 @@
 package dev.vinothm.algorithms.tree;
 
 import java.util.List;
+import java.util.Stack;
 
-public class Utils<T> {
-  public List<T> inOrder(Node<T> tree, List<T> result) {
-    if (tree != null) {
-        inOrder(tree.left, result);
-        result.add(tree.value);
-        inOrder(tree.right, result);
-    }
-    return result;
-  }
+public class Utils {
+	public List<Integer> inOrder(Node tree, List<Integer> result) {
+		if (tree != null) {
+			inOrder(tree.left, result);
+			result.add(tree.value);
+			inOrder(tree.right, result);
+		}
+		return result;
+	}
 
-  public List<T> preOrder(Node<T> tree, List<T> result) {
-    if (tree != null) {
-        result.add(tree.value);
-        preOrder(tree.left, result);
-        preOrder(tree.right, result);
-    }
-    return result;
-  }
+	public List<Integer> preOrder(Node tree, List<Integer> result) {
+		if (tree != null) {
+			result.add(tree.value);
+			preOrder(tree.left, result);
+			preOrder(tree.right, result);
+		}
+		return result;
+	}
 
-  public List<T> postOrder(Node<T> tree, List<T> result) {
-    if (tree != null) {
-	    postOrder(tree.left, result);
-	    postOrder(tree.right, result);
-	    result.add(tree.value);
-    }
-    return result;
-  }
+	public List<Integer> postOrder(Node tree, List<Integer> result) {
+		if (tree != null) {
+			postOrder(tree.left, result);
+			postOrder(tree.right, result);
+			result.add(tree.value);
+		}
+		return result;
+	}
+
+	public List<Integer> inOrderUsingStack(Node tree, List<Integer> result) {
+		var stack = new Stack<Node>();
+		while (stack.size() > 0 || tree != null) {
+			while (tree != null) {
+				stack.push(tree);
+				tree = tree.left;
+			}
+
+			tree = stack.pop();
+			result.add(tree.value);
+			tree = tree.right;
+		}
+		return result;
+	}
+
+	public Node minimum(Node tree) {
+		while (tree.left != null) {
+			tree = tree.left;
+		}
+		return tree;
+	}
+
+	public Node find(Node tree, int data) {
+		if (tree == null || tree.value == data) {
+			return tree;
+		} else if (tree.value > data) {
+			return find(tree.left, data);
+		} else {
+			return find(tree.right, data);
+		}
+	}
+
+	public Node successor(Node tree, int data) {
+		Node node = find(tree, data);
+		if (node != null && node.right != null) {
+			return minimum(node.right);
+		}
+		Node succ = null;
+		while (tree != null && node != null && node.value != tree.value) {
+
+			if (node.value < tree.value) {
+				succ = tree;
+				tree = tree.left;
+			} else {
+				tree = tree.right;
+			}
+
+		}
+		return succ;
+	}
+
+	public Node insert(Node tree, int data) {
+		if (tree == null)
+			return new Node(data, null, null);
+		if (tree.value > data) {
+			tree.left = insert(tree.left, data);
+			return tree;
+		}
+		tree.right = insert(tree.right, data);
+		return tree;
+	}
+
+	public Node build(int... data) {
+		Node root = null;
+		if (data.length > 0) {
+			root = new Node(data[0], null, null);
+			for (int i = 1; i < data.length; i++) {
+				insert(root, data[i]);
+			}
+		}
+		return root;
+	}
 }
--- algorithms/src/test/java/dev/vinothm/algorithms/tree/UtilsTest.java
+++ algorithms/src/test/java/dev/vinothm/algorithms/tree/UtilsTest.java
@@ -12,22 +12,44 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UtilsTest {
 
-	Node<Integer> testTree = null;
-	Utils<Integer> treeUtils = new Utils<Integer>();
+	Node testTree = null;
+	Utils treeUtils = new Utils();
 
 	@BeforeAll
 	void init() {
-		this.testTree = new Node<Integer>(1, null, null);
-		testTree.left = new Node<Integer>(2, null, null);
-		testTree.right = new Node<Integer>(3, null, null);
-		testTree.left.left = new Node<Integer>(4, null, null);
-		testTree.left.right = new Node<Integer>(5, null, null);
+		this.testTree = treeUtils.build(new int[] { 2, 4, 3, 6, 7, 13, 9, 15, 17, 18, 20 });
 	}
 
 	@Test
-	void testTraversals() {
-		assertEquals(treeUtils.preOrder(testTree, new ArrayList<Integer>()), List.of(1, 2, 4, 5, 3));
-		assertEquals(treeUtils.inOrder(testTree, new ArrayList<Integer>()), List.of(4, 2, 5, 1, 3));
-		assertEquals(treeUtils.postOrder(testTree, new ArrayList<Integer>()), List.of(4, 5, 2, 3, 1));
+	void testInorderTraversals() {
+		assertEquals(List.of(2, 3, 4, 6, 7, 9, 13, 15, 17, 18, 20),
+				treeUtils.inOrder(testTree, new ArrayList<Integer>()));
+		assertEquals(List.of(2, 3, 4, 6, 7, 9, 13, 15, 17, 18, 20),
+				treeUtils.inOrderUsingStack(testTree, new ArrayList<Integer>()));
+	}
+
+	@Test
+	void testPreorderTraversals() {
+		assertEquals(List.of(15, 6, 3, 2, 4, 7, 13, 9, 18, 17, 20),
+				treeUtils.preOrder(testTree, new ArrayList<Integer>()));
+	}
+
+	@Test
+	void testPostorderTraversals() {
+		assertEquals(List.of(2, 4, 3, 9, 13, 7, 6, 17, 20, 18, 15),
+				treeUtils.postOrder(testTree, new ArrayList<Integer>()));
+	}
+
+	@Test
+	void testMin() {
+		Node testTree = treeUtils.build(new int[] { 2, 4, 3, 6, 7, 13, 9, 15, 17, 18, 20 });
+		assertEquals(2, treeUtils.minimum(testTree).value);
+	}
+
+	@Test
+	void successor() {
+		Node testTree = treeUtils.build(new int[] { 2, 4, 3, 6, 7, 13, 9, 15, 17, 18, 20 });
+		assertEquals(15, treeUtils.successor(testTree, 13).value);
+		assertEquals(17, treeUtils.successor(testTree, 15).value);
 	}
 }
